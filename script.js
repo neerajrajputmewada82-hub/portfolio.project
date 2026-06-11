a2d9ba883cdec1c9dc03dafcbcc3202c93d20c43
Your Name <neerajrajputmewada82@gmail.com>
2026-06-11 22:54:40 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('#menu a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
